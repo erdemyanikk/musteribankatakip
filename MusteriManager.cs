2d7209a5b913f90210629f5d007b33df4cc1815b
Erdem yanık <138898030+erdemyanikk@users.noreply.github.com>
2024-05-03 16:33:36 +0300

    Add files via upload

---
+++ MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void Siralama(Musteri[] musteri)
+        {
+            foreach (Musteri musteriler in musteri)
+            {
+                Console.WriteLine(musteriler.Name + "\n" + musteriler.Surname + "\n" + musteriler.ID);
+                Console.WriteLine("----------------------------------------");
+            }
+        }
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name + " " + musteri.Surname + " " + musteri.ID + " " + " Eklendi");
+        }
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name + " " + musteri.Surname + " " + musteri.ID + " " + " Silindi");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+
+            musteri.Name = "Ahmet";
+            musteri.Surname = "Aslan";
+            musteri.ID = 187794;
+
+            Musteri musteri2 = new Musteri();
+
+            musteri2.Name = "Mehmet";
+            musteri2.Surname = "Ali";
+            musteri2.ID = 292542;
+
+            Musteri musteri3 = new Musteri();
+
+            musteri3.Name = "Akıl";
+            musteri3.Surname = "Kaya";
+            musteri3.ID = 321313;
+
+            MusteriManager Yonetim = new MusteriManager();
+            Musteri[] musteriler = new Musteri[] { musteri, musteri2, musteri3 };
+            Yonetim.Siralama(musteriler);
+            Yonetim.Ekle(musteri);
+            Yonetim.Silme(musteri3);
+
+            Console.ReadLine();
+        }
+    }
+}
